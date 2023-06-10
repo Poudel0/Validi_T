32e357834a795fb310131c04b29abd145db0e00e
poudel0 <poudel0sandesh@gmail.com>
2023-06-11 04:29:28 +0545

    MOst basic version that can be

--- src/index.sol
+++ src/index.sol
@@ -1,22 +1,22 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity 0.8.;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.8;
 
 contract WEE {
- struct Work {
+    struct Work {
         address a_where;
         string s_where;
         string s_what;
         uint256 start_timestamp;
         uint256 end_timestamp;
     }
-struct Education {
+    struct Education {
         address a_where;
         string s_where;
         string s_what;
         uint256 start_timestamp;
         uint256 end_timestamp;
     }
-struct Experience {
+    struct Experience {
         address a_where;
         string s_where;
         string s_what;
@@ -48,7 +48,7 @@ struct Experience {
         Workmap[whose].start_timestamp = start;
         Workmap[whose].end_timestamp = end;
     }
-    
+
     function setEducation(
         address whose,
         address where,
@@ -63,7 +63,7 @@ struct Experience {
         Educationmap[whose].start_timestamp = start;
         Educationmap[whose].end_timestamp = end;
     }
-    
+
     function setExperience(
         address whose,
         address where,
@@ -81,39 +81,83 @@ struct Experience {
 
     //Getter functions
 
-    function getWork(address whose)  public returns (address, address, string memory, string memory,uint256,uint256) {
-        address ad_where = Workmap[whose].a_where;
-        string memory st_where = Workmap[whose].s_where;
-        string memory st_what = Workmap[whose].s_what;
-        uint256 start_time = Workmap[whose].start_timestamp;
-        uint256 end_time = Workmap[whose].end_timestamp;
-        
-
-
-        
+    function getWork(
+        address whose
+    )
+        public
+        view
+        returns (
+            address ad_where,
+            string memory st_where,
+            string memory st_what,
+            uint256 start_time,
+            uint256 end_time
+        )
+    {
+        ad_where = Workmap[whose].a_where;
+        st_where = Workmap[whose].s_where;
+        st_what = Workmap[whose].s_what;
+        start_time = Workmap[whose].start_timestamp;
+        end_time = Workmap[whose].end_timestamp;
     }
 
-
-
-
-    function geta_where(address whose) public view returns (address) {
-        return WORKmap[whose].a_where;
+    function getEducation(
+        address whose
+    )
+        public
+        view
+        returns (
+            address ad_where,
+            string memory st_where,
+            string memory st_what,
+            uint256 start_time,
+            uint256 end_time
+        )
+    {
+        ad_where = Educationmap[whose].a_where;
+        st_where = Educationmap[whose].s_where;
+        st_what = Educationmap[whose].s_what;
+        start_time = Educationmap[whose].start_timestamp;
+        end_time = Educationmap[whose].end_timestamp;
     }
 
-    function gets_where(address whose) public view returns (string memory) {
-        return WORKmap[whose].s_where;
+    function getExperience(
+        address whose
+    )
+        public
+        view
+        returns (
+            address ad_where,
+            string memory st_where,
+            string memory st_what,
+            uint256 start_time,
+            uint256 end_time
+        )
+    {
+        ad_where = Experiencemap[whose].a_where;
+        st_where = Experiencemap[whose].s_where;
+        st_what = Experiencemap[whose].s_what;
+        start_time = Experiencemap[whose].start_timestamp;
+        end_time = Experiencemap[whose].end_timestamp;
     }
 
-    function gets_what(address whose) public view returns (string memory) {
-        return WORKmap[whose].s_what;
-    }
+    // function geta_where(address whose) public view returns (address) {
+    //     return WORKmap[whose].a_where;
+    // }
 
-    function get_start(address whose) public view returns (uint256) {
-        return WORKmap[whose].start_timestamp;
-    }
+    // function gets_where(address whose) public view returns (string memory) {
+    //     return WORKmap[whose].s_where;
+    // }
 
-    function get_end(address whose) public view returns (uint256) {
-        return WORKmap[whose].end_timestamp;
-    }
+    // function gets_what(address whose) public view returns (string memory) {
+    //     return WORKmap[whose].s_what;
+    // }
 
+    // function get_start(address whose) public view returns (uint256) {
+    //     return WORKmap[whose].start_timestamp;
+    // }
+
+    // function get_end(address whose) public view returns (uint256) {
+    //     return WORKmap[whose].end_timestamp;
+    // }
 }
