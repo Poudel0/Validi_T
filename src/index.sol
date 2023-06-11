5773f52dc4f2e53a129af20ce2f0404ec9f8a11d
poudel0 <poudel0sandesh@gmail.com>
2023-06-11 06:44:21 +0545

    V0.1

--- src/index.sol
+++ src/index.sol
@@ -1,13 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.8;
 
-contract WEE {
+// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract WEE is AccessControl {
+    bytes32 private constant INSTITUTION = keccak256("INSTITUTION");
+    bytes32 private constant INDIVIDUAL = keccak256("INDIVIDUAL");
+
     struct Work {
         address a_where;
         string s_where;
         string s_what;
         uint256 start_timestamp;
         uint256 end_timestamp;
+        bool isverified;
     }
     struct Education {
         address a_where;
@@ -15,6 +22,7 @@ contract WEE {
         string s_what;
         uint256 start_timestamp;
         uint256 end_timestamp;
+        bool isverified;
     }
     struct Experience {
         address a_where;
@@ -22,10 +30,13 @@ contract WEE {
         string s_what;
         uint256 start_timestamp;
         uint256 end_timestamp;
+        bool isverified;
     }
-    // constructor() {
 
-    // }
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(INSTITUTION, msg.sender);
+    }
 
     // Mapping defined
     mapping(address => Work) public Workmap;
@@ -41,7 +52,7 @@ contract WEE {
         string memory what,
         uint256 start,
         uint256 end
-    ) public {
+    ) public onlyRole(INDIVIDUAL) {
         Workmap[whose].a_where = where;
         Workmap[whose].s_where = swhere;
         Workmap[whose].s_what = what;
@@ -56,7 +67,7 @@ contract WEE {
         string memory what,
         uint256 start,
         uint256 end
-    ) public {
+    ) public onlyRole(INDIVIDUAL) {
         Educationmap[whose].a_where = where;
         Educationmap[whose].s_where = swhere;
         Educationmap[whose].s_what = what;
@@ -71,7 +82,7 @@ contract WEE {
         string memory what,
         uint256 start,
         uint256 end
-    ) public {
+    ) public onlyRole(INDIVIDUAL) {
         Experiencemap[whose].a_where = where;
         Experiencemap[whose].s_where = swhere;
         Experiencemap[whose].s_what = what;
@@ -91,7 +102,8 @@ contract WEE {
             string memory st_where,
             string memory st_what,
             uint256 start_time,
-            uint256 end_time
+            uint256 end_time,
+            bool verified
         )
     {
         ad_where = Workmap[whose].a_where;
@@ -99,6 +111,7 @@ contract WEE {
         st_what = Workmap[whose].s_what;
         start_time = Workmap[whose].start_timestamp;
         end_time = Workmap[whose].end_timestamp;
+        verified = Workmap[whose].isverified;
     }
 
     function getEducation(
@@ -111,7 +124,8 @@ contract WEE {
             string memory st_where,
             string memory st_what,
             uint256 start_time,
-            uint256 end_time
+            uint256 end_time,
+            bool verified
         )
     {
         ad_where = Educationmap[whose].a_where;
@@ -119,6 +133,7 @@ contract WEE {
         st_what = Educationmap[whose].s_what;
         start_time = Educationmap[whose].start_timestamp;
         end_time = Educationmap[whose].end_timestamp;
+        verified = Educationmap[whose].isverified;
     }
 
     function getExperience(
@@ -131,7 +146,8 @@ contract WEE {
             string memory st_where,
             string memory st_what,
             uint256 start_time,
-            uint256 end_time
+            uint256 end_time,
+            bool verified
         )
     {
         ad_where = Experiencemap[whose].a_where;
@@ -139,25 +155,30 @@ contract WEE {
         st_what = Experiencemap[whose].s_what;
         start_time = Experiencemap[whose].start_timestamp;
         end_time = Experiencemap[whose].end_timestamp;
+        verified = Experiencemap[whose].isverified;
     }
 
-    // function geta_where(address whose) public view returns (address) {
-    //     return WORKmap[whose].a_where;
-    // }
+    function verifyEducation(
+        address whose
+    ) public onlyRole(INSTITUTION) returns (bool) {
+        require(Educationmap[whose].a_where == msg.sender);
+        Educationmap[whose].isverified = true;
+        return true;
+    }
 
-    // function gets_where(address whose) public view returns (string memory) {
-    //     return WORKmap[whose].s_where;
-    // }
+    function verifyWork(
+        address whose
+    ) public onlyRole(INSTITUTION) returns (bool) {
+        require(Workmap[whose].a_where == msg.sender);
+        Workmap[whose].isverified = true;
+        return true;
+    }
 
-    // function gets_what(address whose) public view returns (string memory) {
-    //     return WORKmap[whose].s_what;
-    // }
-
-    // function get_start(address whose) public view returns (uint256) {
-    //     return WORKmap[whose].start_timestamp;
-    // }
-
-    // function get_end(address whose) public view returns (uint256) {
-    //     return WORKmap[whose].end_timestamp;
-    // }
+    function verifyExperience(
+        address whose
+    ) public onlyRole(INSTITUTION) returns (bool) {
+        require(Experiencemap[whose].a_where == msg.sender);
+        Experiencemap[whose].isverified = true;
+        return true;
+    }
 }
